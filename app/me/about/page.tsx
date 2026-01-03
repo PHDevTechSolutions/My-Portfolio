66dc6696a11d9672be1ae9242467c1d290ebd1b7
PHDevTechSolutions <phdevtechsolutions@gmail.com>
2026-01-03 23:26:39 +0800

    Update page.tsx

--- app/me/about/page.tsx
+++ app/me/about/page.tsx
@@ -140,7 +140,7 @@ const footerLinks = [
 ];
 
 const socialLinksFooter = [
-    { icon: Facebook, label: "Facebook", href: "#" },
+    { icon: Facebook, label: "Facebook", href: "https://www.facebook.com/share/1BBoXkoDhz/" },
 ];
 
 export default function AboutUsPage() {
