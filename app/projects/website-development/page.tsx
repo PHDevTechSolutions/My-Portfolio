e1bfc33982944954a029fa0b30b6e3d96e09ce40
PHDevTechSolutions <phdevtechsolutions@gmail.com>
2026-01-03 23:27:37 +0800

    Update page.tsx

--- app/projects/website-development/page.tsx
+++ app/projects/website-development/page.tsx
@@ -161,7 +161,7 @@ const footerLinks = [
 ];
 
 const socialLinksFooter = [
-    { icon: Facebook, label: "Facebook", href: "#" },
+    { icon: Facebook, label: "Facebook", href: "https://www.facebook.com/share/1BBoXkoDhz/" },
 ];
 
 function WebsiteCard({ title, description, tags, image, links, develop, assign, className }: WebsiteCardProps) {
